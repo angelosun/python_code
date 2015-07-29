7185eedd18a96851cc19fce97247f81e72c5cd69
angelosun <angelosun82@gmail.com>
2015-07-30 00:03:27 +0800

    1.0

--- weather.py
+++ weather.py
@@ -1,6 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+
 response = requests.get('http://pm25.in/hangzhou')
-#print(response.text)
+
 soup = BeautifulSoup(response.text, "html.parser")
-print(soup.tbody.tr.td)     
+tab = soup.find('table', attrs={'id':'detail-data'})
+i = 0
+rows = tab.find_all('tr')
+for row in rows:    
+    if (i == 0):
+        i+= 1
+        continue
+
+    cols = row.find_all('td')[0:3]
+    for ele in cols:
+        print(ele.text.strip())
+    print('####################')
+
