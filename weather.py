46f2f7b44ed9d3043ecfbc1ff07c73f6919d60b0
angelosun <angelosun82@gmail.com>
2015-07-31 23:00:06 +0800

    1.1

--- weather.py
+++ weather.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from pymongo import MongoClient
+import time
 
 
 response = requests.get('http://pm25.in/hangzhou')
@@ -13,9 +14,8 @@ rows = tab.find_all('tr')
 
 client = MongoClient('localhost', 27017)
 db = client['weather']
-weather_hz = db.weather_hangzhou
+weather_hz = db.weather_hz
 
-list = []
 for row in rows:    
     if (i == 0):
         i+= 1
@@ -24,17 +24,10 @@ for row in rows:
     cols = row.find_all('td')[0:3]
     dict = {}
 
+    dict['datetime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
     dict['addr'] = cols[0].text.strip()
     dict['pm25'] = cols[1].text.strip()
     weather_hz.insert_one(dict)
     #json_str = json.dumps(dict,skipkeys=True, encoding="utf-8")
-
-    
-    
-
-
-
-
-
 
 
