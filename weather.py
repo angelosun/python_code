e4276908ee436a6617d08522e3c529051a18f808
angelosun <angelosun82@gmail.com>
2015-08-10 00:24:36 +0800

    2.0

--- weather.py
+++ weather.py
@@ -5,36 +5,36 @@ from pymongo import MongoClient
 import time
 import threading
 
-
 def scrapy():
-    response = requests.get('http://pm25.in/hangzhou')
+    while (True):
+        response = requests.get('http://pm25.in/hangzhou')
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    tab = soup.find('table', attrs={'id':'detail-data'})
-    i = 0
-    rows = tab.find_all('tr')
+        soup = BeautifulSoup(response.text, "html.parser")
+        tab = soup.find('table', attrs={'id':'detail-data'})
+        i = 0
+        rows = tab.find_all('tr')
 
-    client = MongoClient('localhost', 27017)
-    db = client['weather']
-    weather_hz = db.weather_hz
+        client = MongoClient('localhost', 27017)
+        db = client['weather']
+        weather_hz = db.weather_hz
 
-    for row in rows:    
-        if (i == 0):
-            i+= 1
-            continue
+        for row in rows:    
+            if (i == 0):
+                i+= 1
+                continue
 
-        cols = row.find_all('td')[0:3]
-        dict = {}
+            cols = row.find_all('td')[0:3]
+            dict = {}
 
-        dict['datetime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
-        dict['addr'] = cols[0].text.strip()
-        dict['pm25'] = cols[1].text.strip()
-        weather_hz.insert_one(dict)
-    #json_str = json.dumps(dict,skipkeys=True, encoding="utf-8")
-    time.sleep(7200)
+            dict['datetime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            dict['addr'] = cols[0].text.strip()
+            dict['pm25'] = cols[1].text.strip()
+            weather_hz.insert_one(dict)
+        #json_str = json.dumps(dict,skipkeys=True, encoding="utf-8")
+        print("finish insert data into mongodb")
+        time.sleep(7200)
+
 if __name__ == '__main__':
-    scrapy()
     t = threading.Thread(target=scrapy)
-    t.setDaemon(True)
-    t.start
+    t.start()
 
